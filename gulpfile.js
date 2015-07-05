259d34082cd8d5d81ad515aca5518ee12ea1412d
Farhad Agzamov <farhad@thisistommy.com>
2015-07-05 21:39:42 +0100

    fixing build process

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@ var minifyCss 	= require('gulp-minify-css');
 var concat 		  = require('gulp-concat');
 var uglify 		  = require('gulp-uglify');
 var del         = require('del');
-var rename = require('gulp-rename');
+var rename      = require('gulp-rename');
+var concatCss   = require('gulp-concat-css');
 
 gulp.task('test', function(){
 	console.log('This is simply a test');
@@ -14,7 +15,6 @@ gulp.task('test', function(){
 
 gulp.task('concatScripts', function() {
    return	gulp.src([
-  		'js/vendor/modernizr-2.6.2-respond-1.1.0.min.js', 
   		'js/vendor/jquery-1.11.0.min.js', 
   		'js/vendor/bootstrap.min.js', 
   		'js/vendor/imagesloaded.pkgd.js', 
@@ -23,28 +23,33 @@ gulp.task('concatScripts', function() {
     .pipe(maps.init())
     .pipe(concat('all.js'))
     .pipe(maps.write('./'))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('js'));
 });
 
 gulp.task("minifyScripts", ["concatScripts"], function() {
-  return gulp.src("js/app.js")
+  return gulp.src("js/all.js")
     .pipe(uglify())
-    .pipe(rename('app.min.js'))
+    .pipe(rename('all.min.js'))
     .pipe(gulp.dest('js'));
 });
 
 gulp.task('watchFiles', function() {
-  gulp.watch('js/main.js', ['concatScripts']);
-})
+  gulp.watch('js/main.js', ['minifyScripts']);
+});
 
 gulp.task('clean', function() {
-  del(['dist', 'css/application.css*', 'js/app*.js*']);
+  del(['build']);
 });
 
 gulp.task("build", ['minifyScripts'], function() {
-  return gulp.src(["css/application.css", "js/app.min.js", 'index.html',
-                   "img/**", "fonts/**"], { base: './'})
-            .pipe(gulp.dest('dist'));
+  return gulp.src([
+    'index.html', 
+    'rcats.ico', 
+    'css/**', 
+    'js/all.min.js',
+    'img/**', 
+    'fonts/**'], { base: './'})
+            .pipe(gulp.dest('build'));
 });
 
 gulp.task('serve', ['watchFiles']);
